56edfc25443a3abae18de3941614dcb6f62a5fc4
soonr <soonr@users.noreply.github.com>
2015-07-10 14:25:15 +0200

    - removing building host libraries

--- Apps.mk
+++ Apps.mk
@@ -14,12 +14,3 @@ include $(LOCAL_PATH)/Apps-config-target.mk
 include $(LOCAL_PATH)/android-config.mk
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/android-config.mk $(LOCAL_PATH)/Apps.mk
 include $(BUILD_EXECUTABLE)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := openssl
-LOCAL_MODULE_TAGS := optional
-LOCAL_SHARED_LIBRARIES := libssl-host libcrypto-host
-include $(LOCAL_PATH)/Apps-config-host.mk
-include $(LOCAL_PATH)/android-config.mk
-LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/android-config.mk $(LOCAL_PATH)/Apps.mk
-include $(BUILD_HOST_EXECUTABLE)
