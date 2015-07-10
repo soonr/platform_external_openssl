236dcbb4e08cf409a9880e5716f2abab5fa90632
soonr <soonr@users.noreply.github.com>
2015-07-10 14:40:30 +0200

    - removing building host libraries

--- Crypto.mk
+++ Crypto.mk
@@ -3,6 +3,7 @@
 include $(CLEAR_VARS)
 LOCAL_SHARED_LIBRARIES := $(log_shared_libraries)
 LOCAL_C_INCLUDES := $(log_c_includes)
+LOCAL_EXPORT_C_INCLUDES := $(log_c_includes)
 
 # The static library should be used in only unbundled apps
 # and we don't have clang in unbundled build yet.
@@ -13,6 +14,8 @@ LOCAL_MODULE := libcrypto_static
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/android-config.mk $(LOCAL_PATH)/Crypto.mk
 include $(LOCAL_PATH)/Crypto-config-target.mk
 include $(LOCAL_PATH)/android-config.mk
+
+LOCAL_SRC_FILES := $(LOCAL_SRC_FILES_$(TARGET_ARCH_ABI))
 
 # Replace cflags with static-specific cflags so we dont build in libdl deps
 LOCAL_CFLAGS_32 := $(openssl_cflags_static_32)
